cfd4444937aaed6de0817a51243ecc7467587fec
Nikhil Sharma <promaxchessace@gmail.com>
2023-09-17 00:05:30 +0530

    header updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
 
 type Props = {}
 
 function Header({}: Props) {
   return (
-    <div></div>
+    <div className=''>
+        <a href="/" className=''>Keep it here.</a>
+        <nav className=''>
+            <ul className=''>
+                <Link href={"#documents"}><li>Documents</li></Link>
+                <Link href={"#videos"}><li>Videos</li></Link>
+                <Link href={"#links"}><li>Links</li></Link>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
